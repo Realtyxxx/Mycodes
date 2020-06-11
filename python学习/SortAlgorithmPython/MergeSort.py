390f6968615910b7d7e28cd71a94ae32530162fd
Realtyxxx <841344563@qq.com>
2020-06-11 11:36:43 +0800

    “”

--- python学习/SortAlgorithmPython/MergeSort.py
+++ python学习/SortAlgorithmPython/MergeSort.py
@@ -1,5 +1,5 @@
 def merge_sort(array):
-    def merge_arr(arr_l, arr_r):
+    def merge_arr(arr_l, arr_r):        #合并所有项
         array = []
         while len(arr_l) and len(arr_r):
             if arr_l[0] <= arr_r[0]:
@@ -12,7 +12,7 @@ def merge_sort(array):
             array += arr_r
         return array
  
-    def recursive(array):
+    def recursive(array):   #划分开，直到分到单个元素为一项
         if len(array) == 1:
             return array
         mid = len(array) // 2
