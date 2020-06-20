bce8ece0bf885526196649a1d7bae9a4dee0c654
Realtyxxx <841344563@qq.com>
2020-06-20 16:49:06 +0800

    ""

--- python学习/吴恩达_神经网络和深度学习课程/2_15Broadcasting_example.py
+++ python学习/吴恩达_神经网络和深度学习课程/2_15Broadcasting_example.py
@@ -5,7 +5,6 @@ print(A)
 
 cal = A.sum(axis=0)  #轴表示我希望Python在竖直方向求和-sum vertically,  水平轴 axis=1。
 print(cal)
-percentage = 100 * A / cal.reshape(
-    1, 4
-)  #实际上这里cal已经是一个1x4的矩阵了，后面部分都可以省略即.reshape(1,4)但当我们不完全确定是什么矩阵时候，可以用该命令进行编译，reshape O(1)。
+percentage = 100 * A / cal.reshape(1, 4)
+#实际上这里cal已经是一个1x4的矩阵了，后面部分都可以省略即.reshape(1,4)但当我们不完全确定是什么矩阵时候，可以用该命令进行编译，reshape O(1)。
 print(percentage)
