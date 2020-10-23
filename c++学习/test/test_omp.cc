2ecc474a675c9fd30056f377ae48a13f367e43d9
Realtyxxx <841344563@qq.com>
2020-10-23 11:03:19 +0800

    omp

---
+++ c++学习/test/test_omp.cc
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<omp.h>
+using namespace std;
+int main (int argc, char *argv[]) {
+
+  int th_id, nthreads;
+
+
+#pragma omp parallel private(th_id)
+
+  //th_id is declared above.  It is is specified as private; so each
+  //thread will have its own copy of th_id
+  {
+    th_id = omp_get_thread_num();
+    printf("Hello World from thread %d\n", th_id);
+    return 0;
+  }
