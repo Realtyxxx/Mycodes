25206db271122c4769d4c72e330b5391bae3e644
Realtyxxx <841344563@qq.com>
2020-12-20 20:38:39 +0800

    " "

--- c++学习/test/my_socket/py/client.py
+++ c++学习/test/my_socket/py/client.py
@@ -2,13 +2,16 @@
 
 import socket
 
+
 def client():
-		#创建套接字
-		s = socket.socket()
-		#连接套接字
-		s.connect(('localhost',6666))
-		print('Recv msg: %s, Client: %d' %(s.recv(1024),i))
-		s.close()
+    # 创建套接字
+    s = socket.socket()
+    # 连接套接字
+    s.connect(('localhost', 6666))
+    print('Recv msg: %s, Client: %d' % (s.recv(1024), i))
+    s.close()
+
+
 if __name__ == '__main__':
-		for i in range(10):
-				client()
+    for i in range(10):
+        client()
