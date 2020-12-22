90843f090377b0cba39c971f445a527effe06565
Realtyxxx <841344563@qq.com>
2020-12-22 10:29:06 +0800

    “fix questions ”

---
+++ c++学习/lt_study_algorithm/String/ectopicEffectiveLetterWord.cc
@@ -0,0 +1,51 @@
+// 有效的字母异位词
+// 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+// 示例 1:
+
+// 输入: s = "anagram", t = "nagaram"
+// 输出: true
+// 示例 2:
+
+// 输入: s = "rat", t = "car"
+// 输出: false
+// 说明:
+// 你可以假设字符串只包含小写字母。
+
+// 进阶:
+// 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+
+// 作者：力扣 (LeetCode)
+// 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn96us/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+ public:
+  bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) return false;
+    int s_hash[26] = {0}, t_hash[26] = {0};
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] != t[i]) {
+        s_hash[s[i] - 'a']++;
+        t_hash[t[i] - 'a']++;
+      }
+    }
+    for (int i = 0; i < 26; i++) {
+      if (s_hash[i] != t_hash[i]) return false;
+    }
+    return true;
+  }
+};
+
+int main() {
+  string s, t;
+  cin >> s >> t;
+	Solution tyx;
+	cout << tyx.isAnagram(s,t);
+  return 0;
+}
