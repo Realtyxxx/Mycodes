a469ba0281a3d3689e831adf33a1708543620fd6
Realtyxxx <841344563@qq.com>
2021-01-19 10:26:31 +0800

    " "

--- c++学习/leetcode/21.合并两个有序链表.cpp
+++ c++学习/leetcode/21.合并两个有序链表.cpp
@@ -13,16 +13,17 @@
  * Total Submissions: 571.6K
  * Testcase Example:  '[1,2,4]\n[1,3,4]'
  *
- * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
- * 
- * 
- * 
+ * 将两个升序链表合并为一个新的 升序
+ * 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *
+ *
  * 示例：
- * 
+ *
  * 输入：1->2->4, 1->3->4
  * 输出：1->1->2->3->4->4
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
@@ -36,29 +37,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
-public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
-    {
-        if (l1 == nullptr)
-        {
-            return l2;
-        }
-        else if (l2 == nullptr)
-        {
-            return l1;
-        }
-        else if (l1->val < l2->val)
-        {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        }
-        else
-        {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode * next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    if (l1 == nullptr) {
+      return l2;
+    } else if (l2 == nullptr) {
+      return l1;
+    } else if (l1->val < l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoLists(l1, l2->next);
+      return l2;
     }
+  }
 };
 // @lc code=end
--- c++学习/lt_study_algorithm/List/reverseList.cc
+++ c++学习/lt_study_algorithm/List/reverseList.cc
@@ -50,33 +50,71 @@ class Solution {
   }
 };
 
+class Solution1 {
+ public:
+  ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    if (l1 == nullptr) {
+      return l2;
+    } else if (l2 == nullptr) {
+      return l1;
+    } else if (l1->val < l2->val) {
+      l1->next = mergeTwoLists(l1->next, l2);
+      return l1;
+    } else {
+      l2->next = mergeTwoLists(l1, l2->next);
+      return l2;
+    }
+  }
+};
+
 int main() {
   srand(time(NULL));
 
   ListNode* head = new ListNode(0);
+  ListNode* head1 = new ListNode(0);
   ListNode* curr = head;
+  ListNode* curr1 = head1;
 
   // initialize;
   int n;
   cin >> n;
+  int x, last = 0;
+
   for (int i = 0; i < n; i++) {
-    int x = int(rand() % 101);
+    x = int(rand() % 101);
     curr->next = new ListNode(x);
     curr = curr->next;
+    last = x;
+  }
+  for (int i = 0; i < n - 1; i++) {
+    x = int(rand() % 101);
+    curr1->next = new ListNode(x);
+    curr1 = curr1->next;
+    last = x;
   }
 
   curr = head->next;
+  curr1 = head1->next;
+
+  cout << "first List :" << endl;
   for (int i = 0; i < n; i++) {
     cout << curr->val << " ";
     curr = curr->next;
   }
   cout << endl;
-  
-  Solution myAns;
 
-  curr = myAns.reverseList(head);
+  cout << "second List :" << endl;
+  for (int i = 0; i < n - 1; i++) {
+    cout << curr1->val << " ";
+    curr1 = curr1->next;
+  }
+  cout << endl;
 
-  for (int i = 0; i < n; i++) {
+  Solution1 myAns;
+
+  curr = myAns.mergeTwoLists(head, head1)->next->next;
+
+  for (int i = 0; i < 2 * n - 2; i++) {
     cout << curr->val << " ";
     curr = curr->next;
   }
