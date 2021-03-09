fd477af724424724126ccde0c1b4c6307251b2aa
Realtyxxx <841344563@qq.com>
2021-03-09 13:50:39 +0800

    " "

--- CCF/2020_12/ccf20201202.cc
+++ CCF/2020_12/ccf20201202.cc
@@ -14,10 +14,12 @@ int sum[N];
 set<int> st;
 int _res;
 int main() {
-	cin >> n;
-	for (int i=1; i<=n; ++i) {
-		int a,b;
-		cin >> a >> b;
-		p[i] = make_pair(a,b);
-	}
-}
+  cin >> n;
+  for (int i = 1; i <= n; ++i) {
+    int a, b;
+    cin >> a >> b;
+    p[i] = make_pair(a, b);
+  }
+  sort(p + 1, p + n + 1);
+  for (int i = 1; i <= n; i++) sum[i] = sum[i - 1] + p[i].second;
+  for (int i = 1; i <= n; i++) }
