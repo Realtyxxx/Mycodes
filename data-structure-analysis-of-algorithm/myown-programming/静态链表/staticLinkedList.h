c6b1e718aa7ae6a76d3c1d0987bfd4b592876ecb
realtyxxx <841344563@qq.com>
2021-03-14 01:38:25 +0800

    " "

--- data-structure-analysis-of-algorithm/myown-programming/静态链表/staticLinkedList.h
+++ data-structure-analysis-of-algorithm/myown-programming/静态链表/staticLinkedList.h
@@ -1,15 +1,15 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-typedef struct {
-  int data;
-  int cur;
-} component;
+// typedef struct {
+//   int data;
+//   int cur;
+// } component;
 
-//将结构体数组中所有分量链接到备用链表中
-void reserveArr(component *array);
-//初始化静态链表
-int initArr(component *array);
-//输出函数
-void displayArr(component *array, int body);
-//从备用链表上摘下空闲节点到函数
-int mallocArr(component *array);
+// //将结构体数组中所有分量链接到备用链表中
+// void reserveArr(component *array);
+// //初始化静态链表
+// int initArr(component *array);
+// //输出函数
+// void displayArr(component *array, int body);
+// //从备用链表上摘下空闲节点到函数
+// int mallocArr(component *array);
