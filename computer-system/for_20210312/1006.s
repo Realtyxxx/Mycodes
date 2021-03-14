205beb1f99fa495ab87d2e41f7ca11c906df6122
realtyxxx <841344563@qq.com>
2021-03-13 23:13:52 -0800

    nowork

--- computer-system/for_20210312/1006.s
+++ computer-system/for_20210312/1006.s
@@ -2,14 +2,12 @@
 
 .global _start
 _start:
-    nop 
-    movl $4,%eax
-    
-    movl $1,%ebx
-    movb $0x65, %dh
-    
-    movl $13,%edx
+	nop 
+	mov  $4,%eax
+	movw $1,%bx
+	movb $0xff65, %dh
+	movl $13,%edx
 
-    movl $1,%eax
-    movl $0,%ebx
-    int $0x80
+	movl $1,%eax
+	movl $0,%ebx
+	int $0x80
