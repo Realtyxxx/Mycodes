91dd9ab2e2b28d210f31d8e14585c99f4a49dd60
Realtyxxx <841344563@qq.com>
2021-03-22 13:20:55 +0800

    ccf 20200901

---
+++ c++学习/CCF/2020_09/01.cc
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+
+typedef struct {
+  int idx;
+  int x;
+  int y;
+} pos;
+
+int n, px, py;
+
+bool comp(const pos& a, const pos& b) {
+  return ((a.x - px) * (a.x - px) + (a.y - py) * (a.y - py)) <
+         ((b.x - px) * (b.x - px) + (b.y - py) * (b.y - py));
+}
+
+int main() {
+  std::cin >> n >> px >> py;
+  pos* p = new pos[n];
+  for (int i = 1; i <= n; ++i) {
+    std::cin >> p[i].x >> p[i].y;
+    p[i].idx = i;
+  }
+
+  std::sort(p + 1, p + n, comp);
+
+  // std::cout << "in my input:" << std::endl;
+  // for (int i = 1; i <= n; ++i) {
+  //   std::cout << p[i].idx << " " << p[i].x << " " << p[i].y << std::endl;
+  // }
+
+  for (int i = 1; i <= 3; ++i) {
+    std::cout << p[i].idx << std::endl;
+  }
+  delete[] p;
+  return 0;
+}
