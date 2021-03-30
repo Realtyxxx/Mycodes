b6a57600fce7760d0c566b1ae50487d7552ad86a
Realtyxxx <841344563@qq.com>
2021-03-31 01:44:55 +0800

    embeded

---
+++ embeded/testswap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  int a = 10, b = 0;
+  __asm__(
+      "movl %1,%%eax\n\r"
+      "movl %%eax,%0"
+      : "=r"(b)
+      : "r"(a)
+      : "%eax");
+  printf("Result: %d, %d", a, b);
+  return 0;
+}
