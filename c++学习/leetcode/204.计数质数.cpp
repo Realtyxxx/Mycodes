a636da682d25d3b9eba662f6daf16e1c68fac2ed
Realtyxxx <841344563@qq.com>
2021-04-06 21:47:52 +0800

    " "

--- c++学习/leetcode/204.计数质数.cpp
+++ c++学习/leetcode/204.计数质数.cpp
@@ -51,23 +51,61 @@
 #include <vector>
 
 using namespace std;
+//线性筛
 class Solution {
  public:
   int countPrimes(int n) {
-    vector<int> isPrime(n, 1);
     vector<int> primes;
-    for (int i = 2; i < n; ++i) {
-      if (isPrime[i]) {
+    vector<int> isprime(n, 1);
+    for (int i = 2; i < n; i++) {
+      if (isprime[i]) {
         primes.push_back(i);
       }
-      for (int j = 0; j < primes.size() && i * primes[j] < n; ++j) {
-        isPrime[i * primes[j]] = 0;
+      for (int j = 0; j < primes.size() && primes[j] * i < n; j++) {
+        isprime[i * primes[j]] = 0;
         if (i % primes[j] == 0) break;
       }
     }
     return primes.size();
   }
 };
+
+//埃氏筛
+// class Solution {
+//  public:
+//   int countPrimes(int n) {
+//     vector<int> isprimes(n, 1);
+//     int ans = 0;
+//     for (int i = 2; i < n; i++) {
+//       if (isprimes[i]) {
+//         ans += 1;
+//         if ((long long)i * i < n) {
+//           for (long j = i * i; j < n; j += i) {
+//             isprimes[j] = 0;
+//           }
+//         }
+//       }
+//     }
+//     return ans;
+//   }
+// };
+//枚举
+// class Solution {
+//  public:
+//   bool isprime(int x) {
+//     for (int i = 2; i * i <= x; i++) {
+//       if (x % i == 0) return false;
+//     }
+//     return true;
+//   }
+//   int countPrimes(int n) {
+//     int ans = 0;
+//     for (int i = 2; i < n; i++) {
+//       ans += isprime(i);
+//     }
+//     return ans;
+//   }
+// };
 // @lc code=end
 
 //线性筛法，未完全弄懂，利用vector记录是否为素数，用另一个vector存储已经遍历到的素数
