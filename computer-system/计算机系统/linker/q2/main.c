f7713f9be4550ebd5e23084a24f7b898e3052c3f
realtyxxx <841344563@qq.com>
2021-05-28 15:53:53 +0800

    cs

--- computer-system/计算机系统/linker/q2/main.c
+++ computer-system/计算机系统/linker/q2/main.c
@@ -1,5 +1,5 @@
 int buf[2]={1,2};
-void swap();
+
 int main(){
 swap();
 return 0;
---
+++ computer-system/计算机系统/linker/q2/main.d
@@ -0,0 +1,22 @@
+
+main.o:     file format elf32-i386
+
+
+Disassembly of section .text:
+
+00000000 <main>:
+   0:	8d 4c 24 04          	lea    0x4(%esp),%ecx
+   4:	83 e4 f0             	and    $0xfffffff0,%esp
+   7:	ff 71 fc             	pushl  -0x4(%ecx)
+   a:	55                   	push   %ebp
+   b:	89 e5                	mov    %esp,%ebp
+   d:	51                   	push   %ecx
+   e:	83 ec 04             	sub    $0x4,%esp
+  11:	e8 fc ff ff ff       	call   12 <main+0x12>
+			12: R_386_PC32	swap
+  16:	b8 00 00 00 00       	mov    $0x0,%eax
+  1b:	83 c4 04             	add    $0x4,%esp
+  1e:	59                   	pop    %ecx
+  1f:	5d                   	pop    %ebp
+  20:	8d 61 fc             	lea    -0x4(%ecx),%esp
+  23:	c3                   	ret    
