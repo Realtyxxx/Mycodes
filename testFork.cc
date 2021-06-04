49d2cd297e03c25750046aefcbc0d1ffa2f896fe
realtyxxx <841344563@qq.com>
2021-06-04 11:50:16 +0800

    fork

--- testFork.cc
+++ testFork.cc
@@ -1,14 +1,15 @@
 #include <pthread.h>
 // #include <sys/types.h>
 #include <unistd.h>
+
 #include <iostream>
 using namespace std;
 int main() {
   pid_t pid = fork();
   if (pid == 0) {
-    cout << "call from child";
+    cout << "call from child\n";
   } else {
-    cout << "call from parents";
+    cout << "call from parents\n";
   }
   return 0;
 }
