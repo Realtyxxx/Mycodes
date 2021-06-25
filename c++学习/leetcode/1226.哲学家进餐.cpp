7842c3d31f5bb949cfbc75ee10fc471c2e71da98
realtyxxx <841344563@qq.com>
2021-06-26 00:57:23 +0800

    LC

--- c++学习/leetcode/1226.哲学家进餐.cpp
+++ c++学习/leetcode/1226.哲学家进餐.cpp
@@ -71,62 +71,34 @@
  *
  *
  */
-
-// @lc code=start
 #include <condition_variable>
 #include <mutex>
-class Semaphore {
- public:
-  Semaphore(int count = 0) : count_(count) {}
-
-  void Set(int count) { count_ = count; }
-
-  void Signal() {
-    std::unique_lock<std::mutex> lock(mutex_);
-    ++count_;
-    cv_.notify_one();
-  }
-
-  void Wait() {
-    std::unique_lock<std::mutex> lock(mutex_);
-    while (count_ <= 0) {
-      cv_.wait(lock);
-    }
-    --count_;
-  }
-
- private:
-  int count_;
-  std::mutex mutex_;
-  std::condition_variable cv_;
-};
-
+#include <semaphore>
+// @lc code=start
 class DiningPhilosophers {
  public:
-  DiningPhilosophers() { guid.Set(4); }
+  DiningPhilosophers() {}
 
   void wantsToEat(int philosopher, function<void()> pickLeftFork,
                   function<void()> pickRightFork, function<void()> eat,
                   function<void()> putLeftFork, function<void()> putRightFork) {
     int l = philosopher;
     int r = (philosopher + 1) % 5;
-    guid.Wait();
-
+    guid.lock();
     lock[l].lock();
     lock[r].lock();
     pickRightFork();
     pickLeftFork();
+    guid.unlock();
     eat();
     putRightFork();
     putLeftFork();
     lock[l].unlock();
     lock[r].unlock();
-
-    guid.Signal();
   }
 
  private:
-  Semaphore guid;
+  std::mutex guid;
   std::mutex lock[5];
 };
 // @lc code=end
