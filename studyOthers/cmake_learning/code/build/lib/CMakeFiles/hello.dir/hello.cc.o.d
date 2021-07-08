ec3cb61559f9e4fab72da31d7f3fd308a37758c5
realtyxxx <841344563@qq.com>
2021-07-08 15:43:24 +0800

    cmake learning

---
+++ studyOthers/cmake_learning/code/build/lib/CMakeFiles/hello.dir/hello.cc.o.d
@@ -0,0 +1,16 @@
+lib/CMakeFiles/hello.dir/hello.cc.o: \
+ /home/realtyxxx/mycodes/studyOthers/cmake_learning/code/lib/hello.cc \
+ /usr/include/stdc-predef.h \
+ /home/realtyxxx/mycodes/studyOthers/cmake_learning/code/lib/hello.h \
+ /usr/include/stdio.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/8.2.0/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/local/lib/gcc/x86_64-pc-linux-gnu/8.2.0/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h
