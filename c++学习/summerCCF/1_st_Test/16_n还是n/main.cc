b4ba66c638b0615617987206af52b1ccadf04688
realtyxxx <841344563@qq.com>
2021-07-12 14:35:54 +0800

    summer CCF

--- c++学习/summerCCF/1_st_Test/16_n还是n/main.cc
+++ c++学习/summerCCF/1_st_Test/16_n还是n/main.cc
@@ -19,14 +19,15 @@ bool include(int x, int base) {
 int main() {
   int barrier, base;
   cin >> barrier >> base;
-  vector<int> ans;
+  // vector<int> ans;
   // cout << include(barrier, base) << endl;
   for (int i = 1; i <= barrier; i++) {
     if ((i % base == 0) || include(i, base)) {
-      ans.push_back(i);
+      // ans.push_back(i);
+      cout << i << " ";
     }
   }
-  for (auto a : ans)
-    cout << a << " ";
+  // for (auto a : ans)
+  // cout << a << " ";
   return 0;
 }
